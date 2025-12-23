8a467c1fa62a4e01245388476c827ca9338f5ac6
EddieO23 <eocampo52@gmail.com>
2025-12-23 11:20:39 -0600

    setting up Context for Products and added ProductCard component

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,17 +1,10 @@
-const ProductList = ({products}) => {
+import ProductCard from "./ProductCard";
+
+const ProductList = ({product}) => {
   return ( 
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
       {products.map((product) => (
-        <div className='bg-white rounded-lg shadow p-4 flex flex-col'>
-          <img className='h-40 object-cover rounded mb-4' src={product.image} alt={product.name} />
-        <h2 className="text-xl font-semibold">
-          {product.name}
-        </h2>
-        <p className="text-gray-500 text-sm mb-2">
-          {product.description}
-        </p>
-        <p className="font-bold text-lg">${product.price.toFixed(2)}</p>
-        </div>
+       <ProductCard key={product.id} product={product} />
       ))}
     </div>
    );
---
+++ src/context/ProductContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from 'react';
+
+export const ProductContext = createContext();
+
+export function ProductProvier({ children }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/products');
+        if (!res.ok) throw new Error('Failed to fetch products.');
+
+        const data = await res.json();
+        console.log(data);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <ProductContext.Provider value={{ products, loading, error }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
