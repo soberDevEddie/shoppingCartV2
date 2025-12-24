bdc32aba3651aa48da386b2b1df2d963c6d5a71c
EddieO23 <eocampo52@gmail.com>
2025-12-24 17:37:44 -0600

    added js logic to add item to cart and to check if existing item is already in state then we increase the qty by 1

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,8 @@
+import { useCart } from '../context/CartContext';
 
+const ProductCard = ({ product }) => {
+  const { addToCart } = useCart();
 
-const ProductCard = ({product}) => {
   return (
     <div className='bg-white rounded-lg shadow p-4 flex flex-col'>
       <img
@@ -11,7 +13,12 @@ const ProductCard = ({product}) => {
       <h2 className='text-xl font-semibold'>{product.name}</h2>
       <p className='text-gray-500 text-sm mb-2'>{product.description}</p>
       <p className='font-bold text-lg'>${product.price.toFixed(2)}</p>
-      <button onClick={} className="bg-blue-600 text-white mt-3 px-4 py-2 rounded transition hover:bg-blue-700">Add To Cart</button>
+      <button
+        onClick={() => addToCart(product)}
+        className='bg-blue-600 text-white mt-3 px-4 py-2 rounded transition hover:bg-blue-700'
+      >
+        Add To Cart
+      </button>
     </div>
   );
 };
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,14 +4,23 @@ const CartContext = createContext();
 
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
+
   const addToCart = (product) => {
-    setCart()
-  }
+    setCart((prev) => {
+      const existigProduct = prev.find((item) => item.id === product.id);
+
+      if (existigProduct) {
+        return prev.map((item) =>
+          item.id === product.id ? { ...item, qty: item.qty + 1 } : item
+        );
+      }
+
+      return [...prev, { ...product, qty: 1 }];
+    });
+  };
 
   return (
-    <CartContext.Provider value={{ cart }}>
-      {children}
-    </CartContext.Provider>
+    <CartContext.Provider value={{ cart, addToCart }}>{children}</CartContext.Provider>
   );
 }
 
