89fcda18fa24e2243f77fc90f889b22180c58475
EddieO23 <eocampo52@gmail.com>
2025-12-24 13:39:19 -0600

    added CartContext file

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState, useContext, use } from 'react';
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  return (
+    <CartContext.Provider value={{ cart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
