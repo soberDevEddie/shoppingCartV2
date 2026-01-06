970060484af29f82ea24646dfa0d129554e70213
EddieO23 <eocampo52@gmail.com>
2026-01-06 12:21:13 -0600

    added the ability to store items to local storage

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,9 +1,17 @@
-import { createContext, useState, useContext, use } from 'react';
+import { createContext, useState, useContext, useEffect } from 'react';
 
 const CartContext = createContext();
 
 export function CartProvider({ children }) {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem('cart');
+
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (product) => {
     setCart((prev) => {
